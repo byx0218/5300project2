efc79a889ebcebc9865803340f58db94edcdad0e
Yixuan <xybyx0218@gmail.com>
2016-04-23 22:52:45 -0400

    Commented out useless code.

--- PageRank/src/pagerank/PageRank.java
+++ PageRank/src/pagerank/PageRank.java
@@ -1,16 +1,31 @@
 package pagerank;
 
-import java.util.Map;
-
 public class PageRank {
 	
 	public static void main(String[] args) {
 		EdgeFilter filter = new EdgeFilter();
 		filter.filterEdges();
 		
-		Map<Long, Node> nodeTbl1 = filter.processEdges();
-		Map<Long, Node> nodeTbl2 = filter.processEdges();
-		
+		// test parser OK
+//		Map<Long, Node> nodeTbl1 = filter.processEdges();
+//		Map<Long, Node> nodeTbl2 = filter.processEdges();
+//		
+//		System.out.println(nodeTbl1.size());
+//		System.out.println(nodeTbl2.size());
+//		System.out.println();
+//		
+//		for (Long srcId : nodeTbl1.keySet()) {
+//			Node n1 = nodeTbl1.get(srcId);
+//			
+//			if (nodeTbl2.containsKey(srcId)) {
+//				Node n2 = nodeTbl2.get(srcId);
+//				
+//				if (!n1.getDestinationList().equals(n2.getDestinationList()))
+//					System.out.println("node error: " + srcId);
+//			} else {
+//				System.out.println("key error: " + srcId);
+//			}
+//		}
 	}
 	
 }
