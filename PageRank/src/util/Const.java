ba7de4b6acd1ea31824baafbe81320f79808916f
Yixuan <xybyx0218@gmail.com>
2016-04-26 18:51:59 -0400

    Block parser added. Block ID look up method added to Const class.

--- PageRank/src/util/Const.java
+++ PageRank/src/util/Const.java
@@ -17,4 +17,45 @@ public class Const {
     public static final String DST_IDS_PREFIX = "dstIds";
     public static final String PAGE_RANK_PREFIX = "pr";
     
+    /** Block ID boundary list. */
+    public static final long[] bound =
+            new long[] {10328, 20373, 30629, 40645, 50462, 60841, 70591, 80118,
+                    90497, 100501, 110567, 120945, 130999, 140574, 150953, 161332,
+                    171154, 181514, 191625, 202004, 212383, 222762, 232593, 242878,
+                    252938, 263149, 273210, 283473, 293255, 303043, 313370, 323522,
+                    333883, 343663, 353645, 363929, 374236, 384554, 394929, 404712,
+                    414617, 424747, 434707, 444489, 454285, 464398, 474196, 484050,
+                    493968, 503752, 514131, 524510, 534709, 545088, 555467, 565846,
+                    576225, 586604, 596585, 606367, 616148, 626448, 636240, 646022,
+                    655804, 665666, 675448, 685230};
+    
+    
+    /**
+     * Given a Node ID, return the corresponding Block ID.
+     * @param nodeId Node ID
+     * @return Block ID of the given Node ID
+     */
+    public static long blockIdOfNode(long nodeId) {
+        int lo = 0;
+        int hi = 67;
+        
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            
+            if (nodeId < bound[mid] && (mid == 0 || bound[mid - 1] <= nodeId)) {
+                return (long) mid;
+            }
+            
+            if (nodeId < bound[mid]) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+            
+            mid = (lo + hi) / 2;
+        }
+        
+        return (long) lo;
+    }
+    
 }
--- PageRank/src/util/EdgeFilter.java
+++ PageRank/src/util/EdgeFilter.java
@@ -4,13 +4,16 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class EdgeFilter {
     
     /** Files to read or write. */
     private static final String EDGES = "edges.txt";
+    private static final String BLOCKS = "blocks.txt";
     private static final String FLTRED = "filtered_edges.txt";
     
     /** Filter parameters from NetID "xg95". */
@@ -20,28 +23,62 @@ public class EdgeFilter {
     
     private static final double INIT_PR = 1.0 / util.Const.N;
     
-    public static void main(String[] args) {
-        Map<Long, Node> nodeTbl1 = processEdges();
-        filterEdges();
-        Map<Long, Node> nodeTbl2 = processFilteredEdges();
+//    public static void main(String[] args) {
+//        Map<Long, Node> nodeTbl1 = processEdges();
+//        filterEdges();
+//        Map<Long, Node> nodeTbl2 = processFilteredEdges();
+//        
+//        System.out.println(nodeTbl1.size());
+//        System.out.println(nodeTbl2.size());
+//        
+//        for (Long srcId : nodeTbl1.keySet()) {
+//            if (nodeTbl2.containsKey(srcId)) {
+//                Node n1 = nodeTbl1.get(srcId);
+//                Node n2 = nodeTbl2.get(srcId);
+//                
+//                if (!n1.getDestinationList().equals(n2.getDestinationList())) {
+//                    System.out.println("list error " + srcId);
+//                    break;
+//                }
+//            } else {
+//                System.out.println("key error " + srcId);
+//                break;
+//            }
+//        }
+//    }
+    
+    public static int[] processBlocks() {
+        String line = null;
+        List<Integer> blocks = new ArrayList<Integer>();
         
-        System.out.println(nodeTbl1.size());
-        System.out.println(nodeTbl2.size());
-        
-        for (Long srcId : nodeTbl1.keySet()) {
-            if (nodeTbl2.containsKey(srcId)) {
-                Node n1 = nodeTbl1.get(srcId);
-                Node n2 = nodeTbl2.get(srcId);
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(BLOCKS));
+            
+            while ((line = reader.readLine()) != null) {
+                line = line.trim();
                 
-                if (!n1.getDestinationList().equals(n2.getDestinationList())) {
-                    System.out.println("list error " + srcId);
+                if (line.isEmpty()) {
                     break;
                 }
-            } else {
-                System.out.println("key error " + srcId);
-                break;
+                
+                blocks.add(Integer.parseInt(line));
             }
+            
+            reader.close();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        
+        int nodeId = 0;
+        int[] blockIds = new int[blocks.size()];
+        
+        for (int i = 0; i < blocks.size(); i ++) {
+            nodeId += blocks.get(i);
+            blockIds[i] = nodeId;
+        }
+        
+        return blockIds;
     }
     
     /**
